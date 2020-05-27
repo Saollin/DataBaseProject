4149f1d9c19e11ad96b23129e7a3ecf8c15250a9
Saollin <gregory.janosz@gmail.com>
2020-05-27 12:41:38 +0200

    Correct Group entity

--- src/main/java/edu/agh/project/entities/Group.java
+++ src/main/java/edu/agh/project/entities/Group.java
@@ -22,8 +22,7 @@ public class Group {
     @ManyToMany(cascade = {CascadeType.PERSIST})
     private Set<Student> students;
 
-    @OneToMany(mappedBy = "examinatedGroup", cascade = {CascadeType.PERSIST})
-    @JoinColumn(name="EXAMINATIONS_FK")
+    @OneToMany(mappedBy = "group", cascade = {CascadeType.PERSIST})
     private Set<Examination> examinations;
 
     public Group() {}
@@ -32,5 +31,55 @@ public class Group {
         this.course = course;
         this.teacher = teacher;
         this.groupTime = groupTime;
+    }
+
+    public void addExamination(Examination examination) {
+        examinations.add(examination);
+    }
+
+    public void removeExamination(Examination examination) {
+        examinations.remove(examination);
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+        student.addToGroup(this);
+    }
+
+    public void removeStudent(Student student) {
+        student.removeFromGroup(this);
+        students.remove(student);
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public void setGroupTime(GroupTime groupTime) {
+        this.groupTime = groupTime;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public GroupTime getGroupTime() {
+        return groupTime;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public Set<Examination> getExaminations() {
+        return examinations;
     }
 }
